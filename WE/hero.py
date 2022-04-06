39d4863da9fa46ca1d1a74e8a45112519589436c
Shivam Kumar Shah <102382395+shahshivam1210@users.noreply.github.com>
2022-04-06 15:23:07 +0530

    Delete WE directory

--- WE/hero.py
+++
@@ -1,9 +0,0 @@
-print("isosles triagle")
-side = int(input("input the side of side : \n"))
-print(side)
-base = int(input("input the base sidr : \n"))/2
-print(base)
-height = ((side*side) - (base-base))**0.5
-
-print(height)
-print(f"are of triangle {(base*height)/2}")
